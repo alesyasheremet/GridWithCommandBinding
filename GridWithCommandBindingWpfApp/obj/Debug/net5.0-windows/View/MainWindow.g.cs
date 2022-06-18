2dd8f65347876dc7a2e1ec8272f467f8b316d035
Alesya Sheremet <alesya.sheremet@gmail.com>
2022-06-18 20:12:24 +0200

    dowgraded to .net5

--- GridWithCommandBindingWpfApp/obj/Debug/net5.0-windows/View/MainWindow.g.cs
+++ GridWithCommandBindingWpfApp/obj/Debug/net5.0-windows/View/MainWindow.g.cs
@@ -59,7 +59,7 @@ namespace GridWithCommandBindingWpfApp.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/GridWithCommandBindingWpfApp;V1.0.0.0;component/view/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/GridWithCommandBindingWpfApp;component/view/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
